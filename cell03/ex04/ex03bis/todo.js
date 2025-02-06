f3f9b3803793ce69c94b3673049d2dadd23ec15c
guyzazone2549 <116436226+guyzazone2549@users.noreply.github.com>
2025-02-06 18:40:10 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,53 +1,56 @@
-function loadTasks() {
-    const tasks = getCookie("tasks");
-    if (tasks) {
-        const tasksArray = JSON.parse(tasks);
-        tasksArray.forEach(task => {
-            addTaskToDOM(task);
-        });
-    }
-}
-
-function addTaskToDOM(taskContent) {
-    const taskDiv = $("<div>").text(taskContent);
-    taskDiv.on("click", function() {
-        const confirmation = confirm("Do you want to remove this task?");
-        if (confirmation) {
-            taskDiv.remove();
-            updateTasksCookie();
-        }
-    });
-    $("#ft_list").prepend(taskDiv);
-}
-
-function updateTasksCookie() {
-    const taskArray = [];
-    $("#ft_list div").each(function() {
-        taskArray.push($(this).text());
-    });
-    document.cookie = "tasks=" + JSON.stringify(taskArray) + "; path=/";
-}
-
-function getCookie(name) {
-    const cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookie = cookieArr[i].trim();
-        if (cookie.indexOf(name + "=") == 0) {
-            return cookie.substring(name.length + 1, cookie.length);
-        }
-    }
-    return null;
-}
-
-window.onload = function() {
+$(document).ready(function () {
     loadTasks();
-    $("#bot").on("click", function() {
+
+    $("#bot").click(function () {
         const newTask = prompt("Enter a new task:");
-        if (newTask && newTask.trim() !== "") {
+        if (newTask && $.trim(newTask) !== "") {
             addTaskToDOM(newTask);
             updateTasksCookie();
         } else {
             alert("Task cannot be empty!");
         }
     });
-};
+});
+
+function loadTasks() {
+    const tasks = getCookie("tasks");
+    if (tasks) {
+        try {
+            const tasksArray = JSON.parse(decodeURIComponent(tasks));
+            $.each(tasksArray, function (_, task) {
+                addTaskToDOM(task);
+            });
+        } catch (error) {
+            console.error("Error parsing tasks from cookie:", error);
+        }
+    }
+}
+
+function addTaskToDOM(taskContent) {
+    const $taskDiv = $("<div></div>").text(taskContent);
+    $taskDiv.click(function () {
+        if (confirm("Do you want to remove this task?")) {
+            $(this).remove();
+            updateTasksCookie();
+        }
+    });
+    $("#ft_list").prepend($taskDiv);
+}
+
+function updateTasksCookie() {
+    const taskArray = $("#ft_list div").map(function () {
+        return $(this).text();
+    }).get();
+    document.cookie = "tasks=" + encodeURIComponent(JSON.stringify(taskArray)) + ";";
+}
+
+function getCookie(name) {
+    const cookieArr = document.cookie.split(";");
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookie = $.trim(cookieArr[i]);
+        if (cookie.indexOf(name + "=") === 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
